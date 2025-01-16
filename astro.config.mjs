10c4e09066b2dfa7cd423e530adbb0931fb8289c
kim henry <kimanihenry14489@gmail.com>
2025-01-16 11:14:25 +0300

    Added tags page and tags in each blog post and an rss feed

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,8 +2,12 @@
 import { defineConfig } from 'astro/config';
 import vercel from '@astrojs/vercel';
 
+import preact from '@astrojs/preact';
+
 // https://astro.build/config
 export default defineConfig({
   output: 'server',
   adapter: vercel(),
+  site: "https://astro-blog-tut-seven.vercel.app",
+  integrations: [preact()],
 });
